937029fbafdf0cfdd3d7448251cfdfd9c5e28db1
Matias J. Apablaza <mapablaza@itba.edu.ar>
2023-10-15 04:30:40 -0300

    Added multiple infrastructure files
    - CloudFormation templates for deploying ECS cluster and service
    - ECS task definition for API app
    - Deployment script for publishing API image and a customized nginx image

---
+++ .infra/deploy.sh
@@ -0,0 +1,9 @@
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/n9t4t7x6
+
+docker build -t mindhub-api --build-arg APPLICATION=api-app ..
+docker tag mindhub-api:latest public.ecr.aws/n9t4t7x6/mindhub-api:latest
+docker push public.ecr.aws/n9t4t7x6/mindhub-api:latest
+
+docker build -t nginx --build-arg APPLICATION=api-app nginx
+docker tag nginx:latest public.ecr.aws/n9t4t7x6/nginx:latest
+docker push public.ecr.aws/n9t4t7x6/nginx:latest
